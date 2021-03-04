892ab85474fcc2b621e4d41caca290de5bbb3281
Kennet28 <59662768+Kennet28@users.noreply.github.com>
2021-03-03 22:49:11 -0600

    Avance ticket 2
    Se crea pdf desde el api

---
+++ OpenCoolMart.Api/Controllers/TicketController.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OpenCoolMart.Api.Responses;
+using OpenCoolMart.Domain.DTOs;
+using OpenCoolMart.Domain.Entities;
+using OpenCoolMart.Domain.Interfaces;
+using Wkhtmltopdf.NetCore;
+
+namespace OpenCoolMart.Api.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketController : Controller
+    {
+        private readonly IGeneratePdf _generatePdf;
+        private readonly IVentaService _service;
+        private readonly IMapper _mapper;
+        public TicketController(IGeneratePdf generatePdf,IVentaService service,IMapper mapper)
+        {
+            _generatePdf = generatePdf;
+            _service = service;
+            _mapper = mapper;
+        }
+        [HttpGet("{id:int}")]
+        
+        public async Task<IActionResult> Get(int id)
+        {
+                var venta =await _service.VerVenta(id);
+                var ventaDto = _mapper.Map<Venta, VentaResponseDto>(venta);
+                return await _generatePdf.GetPdf("Reportes/Details.cshtml",ventaDto);
+        }
+    }
+}
--- OpenCoolMart.Gui/Controllers/TicketController.cs
+++ OpenCoolMart.Gui/Controllers/TicketController.cs
@@ -12,21 +12,5 @@ namespace OpenCoolMart.Gui.Controllers
     public class TicketController : Controller
     {
         // GET
-        public async Task<IActionResult> Index(int Id)
-        {
-            if (HttpContext.Session.GetString("Id") != null)
-            {
-                var httpClient = new HttpClient();
-                var Token = HttpContext.Session.GetString("Token");
-                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Token);
-                var Json = await httpClient.GetStringAsync("https://localhost:44315/api/Venta/" + Id);
-                var venta = JsonConvert.DeserializeObject<ApiResponse<VentaResponseDto>>(Json);
-                return View(venta.Data);
-            }
-            else
-            {
-                return RedirectToAction("Index");
-            }
-        }
     }
 }
